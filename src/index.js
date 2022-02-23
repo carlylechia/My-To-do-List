f25de14be1785ecbb33564e8bfa7719725fba072
Chia Carlyle <chiacarlyle@gmail.com>
2022-02-23 06:30:34 +0100

    Complete webpack setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['I', 'finally', 'got', 'it!'], ' ');
+  element.classList.add('title');
 
   return element;
 }
